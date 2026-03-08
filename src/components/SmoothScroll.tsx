85a9b4a3b3c4c7a3d88897fde2ff80d7f189fb40
Peguy Casteloot <contact@peguycasteloot.fr>
2026-03-08 10:11:38 +0800

    chore: update deprecated studio-freight packages to lenis

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactLenis } from '@studio-freight/react-lenis';
+import { ReactLenis } from 'lenis/react';
 import { ReactNode } from 'react';
 
 export default function SmoothScroll({ children }: { children: ReactNode }) {
